4f17866c9d3035813fff51045e1a274bf498fd3a
kelsonic <kgadams93@gmail.com>
2016-06-19 14:22:21 -0700

    Add dot notation to access object properties example

---
+++ objects/dot_notation.js
@@ -0,0 +1,6 @@
+// We have been using dot notation so far in the examples above, here is another example again:​
+​var book = {title: "Ways to Go", pages: 280, bookMark1:"Page 20"};
+​
+​// To access the properties of the book object with dot notation, you do this:​
+console.log ( book.title); // Ways to Go​
+console.log ( book.pages); // 280
