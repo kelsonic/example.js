2c68262de1e2a2f71788694c5062e83ad60e0b11
kelsonic <kgadams93@gmail.com>
2016-06-19 14:23:51 -0700

    Add prototype pattern for creating objects example

---
+++ objects/object_prototype_pattern.js
@@ -0,0 +1,23 @@
+function Fruit () {
+​
+}
+​
+Fruit.prototype.color = "Yellow";
+Fruit.prototype.sweetness = 7;
+Fruit.prototype.fruitName = "Generic Fruit";
+Fruit.prototype.nativeToLand = "USA";
+​
+Fruit.prototype.showName = function () {
+  console.log("This is a " + this.fruitName);
+}
+​
+Fruit.prototype.nativeTo = function () {
+  console.log("Grown in:" + this.nativeToLand);
+}
+
+
+var mangoFruit = new Fruit ();
+mangoFruit.showName(); //​
+mangoFruit.nativeTo();
+​// This is a Generic Fruit​
+​// Grown in:USA
