bccc726fe3cdedfa1f51f17dfd929290a6711ab8
kelsonic <kgadams93@gmail.com>
2016-06-19 14:50:50 -0700

    Add this example with jquery

---
+++ this/this_in_jquery.js
@@ -0,0 +1,7 @@
+// A very common piece of jQuery code​
+​
+$("button").click(function (event) {
+  // $(this) will have the value of the button ($("button")) object​
+  ​// because the button object invokes the click () method​
+  console.log($(this).prop("name"));
+});
