8b61c192059cec21e6da0b091eef1c8db5b566fd
kelsonic <kgadams93@gmail.com>
2016-06-19 14:23:35 -0700

    Add object literal example

---
+++ objects/object_literal.js
@@ -0,0 +1,13 @@
+// This is an empty object initialized using the object literal notation​
+​var myBooks = {};
+​
+​// This is an object with 4 items, again using object literal​
+​var mango = {
+  color: "yellow",
+  shape: "round",
+  sweetness: 8,
+​
+  ​howSweetAmI: function () {
+    console.log("Hmm Hmm Good");
+  }
+}
